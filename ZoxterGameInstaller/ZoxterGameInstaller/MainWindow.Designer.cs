cd274114f9a51098afae20a7aa30a27bffea8ece
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2020-08-22 07:07:30 -0300

    Tweaks and comments

---
+++ ZoxterGameInstaller/ZoxterGameInstaller/MainWindow.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace ZoxterGameInstaller
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.btn_dl = new System.Windows.Forms.Button();
+            this.btn_donate = new System.Windows.Forms.Button();
+            this.btn_dlCancel = new System.Windows.Forms.Button();
+            this.btn_quit = new System.Windows.Forms.Button();
+            this.pb_dlStatus = new System.Windows.Forms.ProgressBar();
+            this.lbl_installPath = new System.Windows.Forms.Label();
+            this.lbl_dlProgress = new System.Windows.Forms.Label();
+            this.btn_directorySwitch = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.lbl_twitter = new System.Windows.Forms.LinkLabel();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.lbl_bytes = new System.Windows.Forms.Label();
+            this.btn_openDirectory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_dl
+            // 
+            this.btn_dl.Location = new System.Drawing.Point(12, 12);
+            this.btn_dl.Name = "btn_dl";
+            this.btn_dl.Size = new System.Drawing.Size(221, 103);
+            this.btn_dl.TabIndex = 0;
+            this.btn_dl.Text = "Download";
+            this.btn_dl.UseVisualStyleBackColor = true;
+            this.btn_dl.Click += new System.EventHandler(this.btn_dl_Click);
+            // 
+            // btn_donate
+            // 
+            this.btn_donate.Location = new System.Drawing.Point(591, 485);
+            this.btn_donate.Name = "btn_donate";
+            this.btn_donate.Size = new System.Drawing.Size(221, 103);
+            this.btn_donate.TabIndex = 1;
+            this.btn_donate.Text = "Donate";
+            this.btn_donate.UseVisualStyleBackColor = true;
+            this.btn_donate.Click += new System.EventHandler(this.btn_donate_Click);
+            // 
+            // btn_dlCancel
+            // 
+            this.btn_dlCancel.Enabled = false;
+            this.btn_dlCancel.Location = new System.Drawing.Point(469, 28);
+            this.btn_dlCancel.Name = "btn_dlCancel";
+            this.btn_dlCancel.Size = new System.Drawing.Size(96, 23);
+            this.btn_dlCancel.TabIndex = 2;
+            this.btn_dlCancel.Text = "Cancel";
+            this.btn_dlCancel.UseVisualStyleBackColor = true;
+            this.btn_dlCancel.Click += new System.EventHandler(this.btn_dlCancel_Click);
+            // 
+            // btn_quit
+            // 
+            this.btn_quit.Location = new System.Drawing.Point(12, 485);
+            this.btn_quit.Name = "btn_quit";
+            this.btn_quit.Size = new System.Drawing.Size(221, 103);
+            this.btn_quit.TabIndex = 3;
+            this.btn_quit.Text = "Quit";
+            this.btn_quit.UseVisualStyleBackColor = true;
+            this.btn_quit.Click += new System.EventHandler(this.btn_quit_Click);
+            // 
+            // pb_dlStatus
+            // 
+            this.pb_dlStatus.Location = new System.Drawing.Point(242, 28);
+            this.pb_dlStatus.Name = "pb_dlStatus";
+            this.pb_dlStatus.Size = new System.Drawing.Size(221, 23);
+            this.pb_dlStatus.TabIndex = 4;
+            // 
+            // lbl_installPath
+            // 
+            this.lbl_installPath.AutoSize = true;
+            this.lbl_installPath.Location = new System.Drawing.Point(239, 102);
+            this.lbl_installPath.Name = "lbl_installPath";
+            this.lbl_installPath.Size = new System.Drawing.Size(190, 13);
+            this.lbl_installPath.TabIndex = 5;
+            this.lbl_installPath.Text = "Installation path: C:\\idk\\pbgame\\game";
+            // 
+            // lbl_dlProgress
+            // 
+            this.lbl_dlProgress.AutoSize = true;
+            this.lbl_dlProgress.Location = new System.Drawing.Point(239, 12);
+            this.lbl_dlProgress.Name = "lbl_dlProgress";
+            this.lbl_dlProgress.Size = new System.Drawing.Size(21, 13);
+            this.lbl_dlProgress.TabIndex = 6;
+            this.lbl_dlProgress.Text = "0%";
+            // 
+            // btn_directorySwitch
+            // 
+            this.btn_directorySwitch.Location = new System.Drawing.Point(242, 57);
+            this.btn_directorySwitch.Name = "btn_directorySwitch";
+            this.btn_directorySwitch.Size = new System.Drawing.Size(162, 23);
+            this.btn_directorySwitch.TabIndex = 7;
+            this.btn_directorySwitch.Text = "Choose directory...";
+            this.btn_directorySwitch.UseVisualStyleBackColor = true;
+            this.btn_directorySwitch.Click += new System.EventHandler(this.btn_directorySwitch_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 121);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(800, 358);
+            this.webBrowser.TabIndex = 8;
+            // 
+            // lbl_twitter
+            // 
+            this.lbl_twitter.AutoSize = true;
+            this.lbl_twitter.Location = new System.Drawing.Point(745, 12);
+            this.lbl_twitter.Name = "lbl_twitter";
+            this.lbl_twitter.Size = new System.Drawing.Size(67, 13);
+            this.lbl_twitter.TabIndex = 9;
+            this.lbl_twitter.TabStop = true;
+            this.lbl_twitter.Text = "@Gann4Life";
+            this.lbl_twitter.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lbl_twitter_LinkClicked);
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog.SelectedPath = "C:\\";
+            this.folderBrowserDialog.HelpRequest += new System.EventHandler(this.folderBrowserDialog_HelpRequest);
+            // 
+            // btn_play
+            // 
+            this.btn_play.Enabled = false;
+            this.btn_play.Location = new System.Drawing.Point(302, 485);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(221, 103);
+            this.btn_play.TabIndex = 10;
+            this.btn_play.Text = "Play";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // lbl_bytes
+            // 
+            this.lbl_bytes.AutoSize = true;
+            this.lbl_bytes.Location = new System.Drawing.Point(250, 33);
+            this.lbl_bytes.Name = "lbl_bytes";
+            this.lbl_bytes.Size = new System.Drawing.Size(40, 13);
+            this.lbl_bytes.TabIndex = 11;
+            this.lbl_bytes.Text = "{bytes}";
+            // 
+            // btn_openDirectory
+            // 
+            this.btn_openDirectory.Location = new System.Drawing.Point(410, 57);
+            this.btn_openDirectory.Name = "btn_openDirectory";
+            this.btn_openDirectory.Size = new System.Drawing.Size(162, 23);
+            this.btn_openDirectory.TabIndex = 12;
+            this.btn_openDirectory.Text = "Open directory...";
+            this.btn_openDirectory.UseVisualStyleBackColor = true;
+            this.btn_openDirectory.Click += new System.EventHandler(this.btn_openDirectory_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(824, 600);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_openDirectory);
+            this.Controls.Add(this.lbl_bytes);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.lbl_twitter);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.btn_directorySwitch);
+            this.Controls.Add(this.lbl_dlProgress);
+            this.Controls.Add(this.lbl_installPath);
+            this.Controls.Add(this.pb_dlStatus);
+            this.Controls.Add(this.btn_quit);
+            this.Controls.Add(this.btn_dlCancel);
+            this.Controls.Add(this.btn_donate);
+            this.Controls.Add(this.btn_dl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Application Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_dl;
+        private System.Windows.Forms.Button btn_donate;
+        private System.Windows.Forms.Button btn_dlCancel;
+        private System.Windows.Forms.Button btn_quit;
+        private System.Windows.Forms.ProgressBar pb_dlStatus;
+        private System.Windows.Forms.Label lbl_installPath;
+        private System.Windows.Forms.Label lbl_dlProgress;
+        private System.Windows.Forms.Button btn_directorySwitch;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.LinkLabel lbl_twitter;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.Label lbl_bytes;
+        private System.Windows.Forms.Button btn_openDirectory;
+    }
+}
+
---
+++ ZoxterGameInstaller/ZoxterGameInstaller/MainWindow.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace ZoxterGameInstaller
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        enum DownloadStage { Unrar, Game, Decompress }
+
+        DownloadStage dlStage;
+
+        string game_url = "http://download1592.mediafire.com/4lg7fcbjckgg/sj9aygyd5n90ynw/Plazma+Burst+-+Early+Alpha+Demo.rar";
+        string decompressor_url = "http://download1338.mediafire.com/ca29attc1cog/mr5all64xhjcjru/rardecompressor.exe";
+        string donation_url = "https://www.paypal.com/donate/?token=rmRf4D4qO9NiOpmbrCZXPH81yvkiGn5c-dT_r26HQbYbTumF0wAcc1mkbtnKbqz8rhgM00&country.x=AR&locale.x=AR";
+        string twitter_url = "https://twitter.com/gann4life";
+
+        /// <summary>Name of the main installation folder</summary>
+        string root_folder_name = "Zoxter Games";
+        /// <summary>Subfolder where game versions are going to be installed</summary>
+        string game_path = "\\versions";
+        /// <summary>Configuration folder for the launcher to use (Unused)</summary>
+        string config_path = "\\config";
+        /// <summary>Return the choosen folder path to install stuff at</summary>
+        string Install_Path(){ return folderBrowserDialog.SelectedPath; }
+        /// <summary>Sets download progress description</summary>
+        string downloadName = "";
+
+        /// <summary>Return main installation folder</summary>
+        string GetRootPath() {
+            return Install_Path() + (Install_Path().Equals(@"C:\") ? "" : "\\") +  root_folder_name;
+        }
+        /// <summary>Return game versions installation folder</summary>
+        string GetGamePath() { return GetRootPath() + game_path; }
+        /// <summary>Return launcher config installation folder (Unused)</summary>
+        string GetConfigPath() { return GetRootPath() + config_path; }
+
+        /// <summary>Get extension of file in URL</summary>
+        string GetURLExtension(string url)
+        {
+            string[] pieces = url.Split('.');
+            return "." + pieces[pieces.Length-1];
+        }
+        /// <summary>Get filename plus its extension from the URL</summary>
+        string GetURLFilename(string url)
+        {
+            string[] pieces = url.Split('/');
+            return pieces[pieces.Length - 1];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var installpath = Environment.GetEnvironmentVariable("installpath", EnvironmentVariableTarget.Machine);
+
+            if(installpath == null)
+            {
+                if (CanRegEdit())
+                {
+                    ChooseNewDirectory();
+                    return;
+                }
+                DialogResult diag = MessageBox.Show($"The application will download and install files at '{GetRootPath()}'. \nIf you want to change it, we require you to run the app in admin mode. \n\nWould you like to use admin mode?", "App permissions", MessageBoxButtons.YesNo);
+                switch(diag)
+                {
+                    case DialogResult.Yes:
+                        OpenInAdminMode();
+                        break;
+                    case DialogResult.No:
+                        DisplayInstallPath();
+                        break;
+                }
+            }
+            else
+            {
+                folderBrowserDialog.SelectedPath = installpath;
+                DisplayInstallPath();
+                if (!Directory.Exists(GetRootPath())) CreateDirectories();
+                if (IsGameInstalled())
+                {
+                    btn_play.Enabled = true;
+                }
+            }
+        }
+
+        private void lbl_twitter_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(twitter_url);
+        }
+
+        private void btn_directorySwitch_Click(object sender, EventArgs e)
+        {
+            ChooseNewDirectory();
+        }
+
+        private void btn_dl_Click(object sender, EventArgs e)
+        {
+            DoGameUpdate();
+        }
+        private void btn_dlCancel_Click(object sender, EventArgs e)
+        {
+            CancelDownload();
+
+            btn_dlCancel.Text = "Cancelled";
+            btn_dlCancel.Enabled = false;
+
+            dlStage = DownloadStage.Unrar;
+        }
+        private void btn_quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btn_donate_Click(object sender, EventArgs e)
+        {
+            Process.Start(donation_url);
+        }
+
+        WebClient client;
+        /// <summary>
+        /// Downloads a file from <b>url</b> and saves it into <b>path</b>
+        /// </summary>
+        /// <param name="url">The <b>URL</b> to download from</param>
+        /// <param name="path">The <b>PATH</b> to save the file at</param>
+        void DownloadFile(string url, string path)
+        {
+            client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadProgressFinished);
+
+            client.DownloadFileAsync(new Uri(url), path + "\\" + GetURLFilename(url));
+
+            btn_dl.Enabled = false;
+
+            btn_dlCancel.Enabled = true;
+            btn_dlCancel.Text = "Cancel";
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            pb_dlStatus.Value = e.ProgressPercentage;
+            lbl_dlProgress.Text = downloadName + " " + e.ProgressPercentage + "%";
+
+            float totalMb = float.Parse(e.TotalBytesToReceive.ToString()) / (1024*1024);
+            float currentMb = float.Parse(e.BytesReceived.ToString()) / (1024 * 1024);
+            lbl_bytes.Text = string.Format("Downloading {0}mb of {1}mb", currentMb.ToString("F2"), totalMb.ToString("F2"));
+        }
+        void client_DownloadProgressFinished(object sender, AsyncCompletedEventArgs e)
+        {
+            pb_dlStatus.Value = 0;
+            btn_dl.Enabled = true;
+            lbl_dlProgress.Text = downloadName + " ---";
+
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Download cancelled.");
+                return;
+            }
+            else
+            {
+                if (dlStage == DownloadStage.Unrar)
+                {
+                    dlStage = DownloadStage.Game;
+                    DoGameUpdate();
+                    return;
+                }
+                else if (dlStage == DownloadStage.Game)
+                {
+                    dlStage = DownloadStage.Decompress;
+                    DoGameUpdate();
+                }
+                else if (dlStage == DownloadStage.Decompress)
+                {
+                    DecompressFiles();
+                    return;
+                }
+            }
+        }
+        /// <summary>
+        /// Decompresses all rar files into versions folder by executing an .exe file
+        /// </summary>
+        void DecompressFiles()
+        {
+            lbl_dlProgress.Text = "Decompressing...";
+
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.UseShellExecute = false;
+            processInfo.WorkingDirectory = GetGamePath();
+            processInfo.FileName = GetGamePath() + "\\rardecompressor.exe";
+
+            Process p = new Process();
+            p.StartInfo = processInfo;
+            p.Start();
+            p.WaitForExit();
+
+            lbl_dlProgress.Text = "Done.";
+            btn_play.Enabled = true;
+        }
+        void CancelDownload()
+        {
+            client.CancelAsync();
+            client = null;
+        }
+        /// <summary>
+        /// Checks if rar files are in versions folder
+        /// </summary>
+        /// <returns></returns>
+        bool RarFilesExist()
+        {
+            string[] dirs = Directory.GetFiles(GetGamePath());
+            bool foundRar = false;
+            foreach(string file in dirs)
+            {
+                if (file.Contains(".rar"))
+                {
+                    foundRar = true;
+                }
+            }
+            return foundRar;
+        }
+        /// <summary>
+        /// Returns a .exe file path
+        /// </summary>
+        /// <returns></returns>
+        string GetExeFile()
+        {
+            string file = "";
+            IEnumerable<string> gameVersions = Directory.EnumerateDirectories(GetGamePath());
+            foreach(string version in gameVersions)
+            {
+                IEnumerable<string> versionFiles = Directory.EnumerateFiles(version);
+                foreach(string f in versionFiles)
+                {
+                    if (f.Contains(".exe"))
+                    {
+                        file = f;
+                        break;
+                    }
+                }
+            }
+            return file;
+        }
+        /// <summary>
+        /// Checks if the game is installed
+        /// </summary>
+        /// <returns></returns>
+        bool IsGameInstalled()
+        {
+            try
+            {
+                IEnumerable<string> versionFolders = Directory.EnumerateDirectories(GetGamePath());
+                return versionFolders.Any();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Download required files and decompresses them if required
+        /// </summary>
+        void DoGameUpdate()
+        {
+            if (RarFilesExist()) // Decompress files
+            {
+                DecompressFiles();
+                MessageBox.Show("Now you can play the game.", "Game installed", MessageBoxButtons.OK);
+                return;
+            }
+            if (client != null && client.IsBusy) return;
+            if (dlStage == DownloadStage.Unrar)
+            {
+                if (File.Exists(GetGamePath() + "\\rardecompressor.exe"))
+                {
+                    dlStage = DownloadStage.Game;
+                    DoGameUpdate();
+                    return;
+                }
+                downloadName = "Unrar tool:";
+                DownloadFile(decompressor_url, GetGamePath());
+            }
+            else if (dlStage == DownloadStage.Game)
+            {
+                dlStage = DownloadStage.Decompress;
+                downloadName = "Main game files";
+                DownloadFile(game_url, GetGamePath());
+            }
+            else if (dlStage == DownloadStage.Decompress)
+            {
+                DecompressFiles();
+                MessageBox.Show("Download finished successfully!");
+            }
+        }
+        /// <summary>
+        /// Prompt a window for the user to choose a new install directory and sets the new path into an Environment variable
+        /// </summary>
+        void ChooseNewDirectory()
+        {
+            if (!CanRegEdit())
+            {
+                DialogResult diag = MessageBox.Show("Amin permissions required to change install directory. \n\nWould you like to enable it?", "Permission denied", MessageBoxButtons.YesNo);
+                switch(diag)
+                {
+                    case DialogResult.Yes:
+                        OpenInAdminMode();
+                        break;
+                    case DialogResult.No:
+                        return;
+                }
+            }
+
+            folderBrowserDialog.ShowDialog();
+            DisplayInstallPath();
+            CreateDirectories();
+
+            SaveInstallPath();
+        }
+        /// <summary>
+        /// Creates required folders for proper installations
+        /// </summary>
+        void CreateDirectories()
+        {
+            Directory.CreateDirectory(GetRootPath());
+            Directory.CreateDirectory(GetGamePath());
+            Directory.CreateDirectory(GetConfigPath());
+        }
+        /// <summary>
+        /// Saves the current installation path into an environment variable
+        /// </summary>
+        void SaveInstallPath()
+        {
+            try
+            {
+                Environment.SetEnvironmentVariable("installpath", Install_Path(), EnvironmentVariableTarget.Machine);
+            }
+            catch (System.Security.SecurityException exc)
+            {
+                MessageBox.Show(exc.ToString());
+            }
+        }
+        /// <summary>
+        /// Checks if the application is able to modify reg files
+        /// </summary>
+        /// <returns></returns>
+        bool CanRegEdit()
+        {
+            try
+            {
+                Environment.SetEnvironmentVariable("installpath", Install_Path(), EnvironmentVariableTarget.Machine);
+                return true;
+            }
+            catch (System.Security.SecurityException)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Open the application with administrator permissions
+        /// </summary>
+        void OpenInAdminMode()
+        {
+            ProcessStartInfo adminProcess = new ProcessStartInfo();
+            adminProcess.UseShellExecute = true;
+            adminProcess.WorkingDirectory = Environment.CurrentDirectory;
+            adminProcess.FileName = Application.ExecutablePath;
+            adminProcess.Verb = "runas";
+            Process.Start(adminProcess);
+            Application.Exit();
+        }
+        /// <summary>
+        /// Displays the current installation directory into the UI
+        /// </summary>
+        void DisplayInstallPath() { lbl_installPath.Text = "Install path: " + GetRootPath(); }
+
+        private void folderBrowserDialog_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+        bool clicking;
+        int clickPointX;
+        int clickPointY;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            clicking = true;
+            clickPointX = e.Location.X;
+            clickPointY = e.Location.Y;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            clicking = false;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clicking)
+            {
+                Point formPos = ActiveForm.Location;
+
+                int cursorX = e.Location.X;
+                int cursorY = e.Location.Y;
+
+                formPos.X += cursorX - (clickPointX);
+                formPos.Y += cursorY - (clickPointY);
+                ActiveForm.Location = formPos;
+            }
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            string gameExe = GetExeFile();
+            if (gameExe == "" || gameExe == null)
+                MessageBox.Show("Could not find any executable. Please install the game.", "Unavailable file", MessageBoxButtons.OK);
+            else
+                Process.Start(gameExe);
+        }
+
+        private void btn_openDirectory_Click(object sender, EventArgs e)
+        {
+            Process.Start(GetRootPath());
+        }
+    }
+}

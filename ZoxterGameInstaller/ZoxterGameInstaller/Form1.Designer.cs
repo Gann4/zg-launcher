c420d756ed75b02e980f2030ac2660804d3d9d63
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2020-08-22 00:31:08 -0300

    Start position is now at center of screen

--- ZoxterGameInstaller/ZoxterGameInstaller/Form1.Designer.cs
+++ ZoxterGameInstaller/ZoxterGameInstaller/Form1.Designer.cs
@@ -188,6 +188,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Application Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
